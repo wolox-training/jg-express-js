6926617dd0c1ef26df09990b1ee929210d550d71
Juan Gutierrez <juanmartin.gutierrez@wolox.com.ar>
2018-08-02 14:30:58 -0300

    User model and migration
    * Added model user 
    
    * Added migration
    
    * Fixes in user model and migration

---
+++ app/models/user.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: { type: DataTypes.STRING, unique: true },
+    password: DataTypes.STRING
+  });
+  return user;
+};
---
+++ migrations/migrations/20180801172022-create-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('users')
+};
