57660882893101fa19729e8414f30fb411596941
JuanGutierrez43 <juanmartin.gutierrez@wolox.com.ar>
2018-08-13 12:51:10 -0300

    fixes with user and generic coding

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user').user,
+  saltRounds = 10,
   validations = require('./validations'),
   bcrypt = require('bcryptjs'),
   logger = require('../logger'),
@@ -18,7 +19,7 @@ const newUser = body => {
   if (errMsg.length > 0) {
     throw errors.invalidUser(errMsg);
   } else {
-    return bcrypt.hash(createUser.password, 10).then(hash => {
+    return bcrypt.hash(createUser.password, saltRounds).then(hash => {
       createUser.password = hash;
       return createUser;
     });
@@ -27,11 +28,10 @@ const newUser = body => {
 
 exports.createUser = (req, res, next) =>
   newUser(req.body)
-    .then(user => {
+    .then(user =>
       User.createUser(user).then(createUser => {
         logger.info(`User correctly created. Welcome, ${createUser.firstName} ${createUser.lastName}`);
         res.status(201);
-      });
-      return newUser;
-    })
+      })
+    )
     .catch(next);
--- app/controllers/validations.js
+++ app/controllers/validations.js
@@ -1,18 +1,16 @@
-exports.validateUser = user => {
-  const errorMsgs = [];
-  const validMail = /([A-Za-z][0-9])|([0-9][A-Za-z])@wolox.com.ar\s*$/;
-  const validPass = /([A-Za-z]+[0-9]+)|([0-9]+[A-Za-z]+)/;
-  const keys = Object.keys(user);
-
-  keys.forEach(function(element) {
-    if (user[element].length <= 0) {
-      errorMsgs.push(`${element} cannot be null or empty`);
-    }
-    if (user[element] === user.email && !user[element].match(validMail)) {
-      errorMsgs.push('Email is not a valid email or not the @wolox.com.ar domain.');
-    }
-    if (user[element] === user.password && !user[element].match(validPass)) {
-      errorMsgs.push('Invalid password. Must be 8 alphanumeric characters or longer.');
+exports.validateUser = object => {
+  const errorMsgs = [],
+    validMail = /([A-Za-z][0-9])|([0-9][A-Za-z])@wolox.com.ar\s*$/,
+    validPass = /([A-Za-z]+[0-9]+)|([0-9]+[A-Za-z]+)/;
+  Object.keys(object).forEach(key => {
+    if (!object[key]) {
+      errorMsgs.push(`${key} cannot be null or empty`);
     }
   });
+  if (!object.email.match(validMail)) {
+    errorMsgs.push('Email is not a valid email or not the @wolox.com.ar domain.');
+  }
+  if (!object.password.match(validPass)) {
+    errorMsgs.push('Invalid password. Must be 8 alphanumeric characters or longer.');
+  }
 };
